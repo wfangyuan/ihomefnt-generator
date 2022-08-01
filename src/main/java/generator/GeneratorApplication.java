66999108328a142318d73428a321ec26471ab4a9
wangfangyuan <201512825@qq.com>
2022-08-01 13:48:53 +0800

    代码生成器修改

---
+++ src/main/java/generator/GeneratorApplication.java
@@ -0,0 +1,16 @@
+package generator;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@MapperScan("generator.dao")
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+}
